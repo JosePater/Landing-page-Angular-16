fc7689b4ba22a77c2673765c97217038f24ca7ad
PaterDev <josepater_1130@hotmail.com>
2023-12-28 17:37:58 -0500

    feat(form): Basic form

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.css']
+  styleUrls: ['./contact.component.css'],
 })
 export class ContactComponent {
 
+  public usuario: any = {
+    name: '',
+    email: '',
+  };
+
+  enviar(): void {
+    console.log(`Nombre: ${this.usuario.name} | Correo: ${this.usuario.email}`);
+  }
 }
