44cc89dacd402cbfde9a09978ed8f884580beeb2
PaterDev <josepater_1130@hotmail.com>
2023-12-28 13:47:26 -0500

    style(navbar): bootstrap navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductsComponent } from './products/products.component';
+import { HomeComponent } from './home/home.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsComponent,
+    HomeComponent,
+    ProductDetailComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
