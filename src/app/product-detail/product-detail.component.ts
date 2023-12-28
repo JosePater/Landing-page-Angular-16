01fe82c68da71ee4f4fa0adf4d07242b22c933ac
PaterDev <josepater_1130@hotmail.com>
2023-12-28 16:40:51 -0500

    chore(wait): Wait a little time with setTimeout

--- src/app/product-detail/product-detail.component.ts
+++ src/app/product-detail/product-detail.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { TypeProduct, productsList } from '../products/products.mock';
+import { timeout } from 'rxjs';
 
 @Component({
   selector: 'app-product-detail',
@@ -9,13 +10,17 @@ import { TypeProduct, productsList } from '../products/products.mock';
 })
 export class ProductDetailComponent implements OnInit {
   
+  loading: boolean = true;
   product?: TypeProduct;
-  
   productList: TypeProduct[] = productsList;
 
   constructor(private _route: ActivatedRoute) {}
 
   ngOnInit(): void {
+    setTimeout(() => {
+      this.loading = false;
+    }, 1500);
+
     this._route.params.subscribe((params) => {
       this.product = this.productList.find(product => product.id == params['productId']);
       // console.log(this.product);
