ced2010279c2882d39700980983dc54c2d73eed1
PaterDev <josepater_1130@hotmail.com>
2024-01-03 12:07:51 -0500

    feat(HttpClient): get one product by Id && error handling (next)

--- src/app/product-detail/product-detail.component.ts
+++ src/app/product-detail/product-detail.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { timeout } from 'rxjs';
+import { ActivatedRoute, Params } from '@angular/router';
 import { IProduct } from '../models/product.model';
 import { ApiService } from '../services/api.service';
 
@@ -20,15 +19,24 @@ export class ProductDetailComponent implements OnInit {
 
   ngOnInit(): void {
     // Ruta
-    this._route.params.subscribe((params) => {
-      this._apiService // Servicio de los métodos de los productos (CREAD)
-        .getProduct(Number(params['productId']))
-        .subscribe((data: IProduct) => {
-          console.log('data: ', data);
-          this.product = data;
-          this.colorValue =
-            (this.product?.price as number) > 200 ? 'red' : 'blue';
-        });
+    this._route.params.subscribe({
+      next: (params: Params) => {
+        this._apiService // Servicio de los métodos de los productos (CREAD)
+          .getProduct(Number(params['productId']))
+          .subscribe({
+            next: (data: IProduct) => {
+              console.log('data: ', data);
+              this.product = data;
+              this.colorValue = (this.product?.price as number) > 200 ? 'red' : 'blue';
+            },
+            error: (error: any) => { // Cuando no hay acceso a Internet
+              console.log('Error de acceso al producto: ', error);
+            },
+          });
+      },
+      error: (error: any) => { // Este error no sucederá
+        console.log('Error de ruta: ', error);
+      },
     });
   }
 }
